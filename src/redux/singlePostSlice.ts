d808364ba83a6f5b834dc5a65f4451b05f202166
Sam N Ting <samnlting@gmail.com>
2023-01-25 15:27:52 -0500

    singlePost slice in progress

---
+++ src/redux/singlePostSlice.ts
@@ -0,0 +1,31 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+// import type {RootState} from "../store";
+
+export const fetchSinglePost = createAsyncThunk(
+  "allPosts/fetchById",
+  async (postID: number) => {
+    try {
+      const response = await fetch(`PLACEHOLDER/allPosts/${postID}`);
+      const data: object = await response.json();
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+);
+
+const singlePostSlice = createSlice({
+  name: "singlePost",
+  initialState: {},
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchSinglePost.fulfilled, (state, action) => {
+      return action.payload;
+    });
+  },
+});
+
+export const singlePostState = (state) => {
+  return state.singlePost;
+};
+export default singlePostSlice.reducer;
