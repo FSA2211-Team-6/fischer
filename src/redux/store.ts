8ed4a2dab67f12e340cd35e371638daae476a149
Derek McEnroe <derek.mcenroe@gmail.com>
2023-01-25 13:30:45 -0600

    added typed dispatch and selector to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import allPostsReducer from "../redux/slices/allPosts";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 export const store = configureStore({
   reducer: {
@@ -11,3 +12,7 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+// Use throughout app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
