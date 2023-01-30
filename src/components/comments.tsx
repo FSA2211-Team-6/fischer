6aff2a6acf1a4bf8ece9de597abd18af3af77452
Sam N Ting <samnlting@gmail.com>
2023-01-30 13:29:07 -0500

    added placeholders for comments and experts components; updated tabs view

---
+++ src/components/comments.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Comments() {
+  return (
+    <>
+      <div>NORMIE COMMENTS HERE</div>
+    </>
+  );
+}
---
+++ src/components/experts.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Experts() {
+  return (
+    <>
+      <div>EXPERT COMMENTS HERE</div>
+    </>
+  );
+}
