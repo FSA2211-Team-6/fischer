c8a9aea62022954156c719bb8348c6bc34df5710
Winter Hardcastle <winter@hardcastle.io>
2023-02-07 23:34:08 -0500

    API route created for searching a post by URL

--- src/pages/api/postsFromUrl.ts
+++ src/pages/api/postsFromUrl.ts
@@ -5,15 +5,19 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
   ) {
-    const URL = req.body.url; 
-    const posts = await prisma.post.findMany({
-      include: {
-        websiteArticle: {
-          where: {
-            articleURL: URL
-          }
-        }
+    const url = req.body.url;
+     
+    const id = await prisma.websiteArticle.findFirst({
+      where: {
+        articleURL: url
       }
     })
+
+    const posts = await prisma.post.findMany({
+      where: {
+        websiteArticleId: id?.id
+      }
+    })
+
     res.status(200).send(posts);
   }
