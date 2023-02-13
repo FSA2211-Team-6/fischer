db2db47654eff9d9c684f6027f64868659ed5e61
Derek McEnroe <derek.mcenroe@gmail.com>
2023-02-13 12:03:22 -0600

    not really a change

--- src/pages/api/postURL.ts
+++ src/pages/api/postURL.ts
@@ -2,23 +2,24 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@/db/prismadb";
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-  ) {
-    const data = JSON.parse(req.body)
-    const url = data.url;
-    
-    const id = await prisma.websiteArticle.findMany({
-      where: {
-        articleURL: url,
-      },
-    })
-    const articleId = id[0].id;
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const data = JSON.parse(req.body);
+  const url = data.url;
 
-    const posts = await prisma.post.findMany({
-      where: {
-        websiteArticleId: articleId
-    }})
+  const id = await prisma.websiteArticle.findMany({
+    where: {
+      articleURL: url,
+    },
+  });
+  const articleId = id[0].id;
 
-    res.status(200).send(posts);
-  }
+  const posts = await prisma.post.findMany({
+    where: {
+      websiteArticleId: articleId,
+    },
+  });
+
+  res.status(200).send(posts);
+}
