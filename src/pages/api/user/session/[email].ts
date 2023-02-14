8c9126b985243e769ea526fc5a85e2b590edb5f5
Edmund Fahrenheit <Fischerprogram@gmail.com>
2023-02-14 02:27:26 -0500

    all necessary pages done. MVP is roughly complete. Must prepare supabase and deploy on Render.

--- src/pages/api/user/session/[email].ts
+++ src/pages/api/user/session/[email].ts
@@ -9,7 +9,17 @@ export default async function handler(
   const email = req.query.email;
 
   const data = await prisma.user.findUnique({
-    where: { email: email },
+    where: { email },
+    include: {
+      comments: true,
+      expertise: {
+        include: {
+          topic: true,
+        },
+      },
+      posts: true,
+      userCompliances: true,
+    },
   });
 
   res.status(200).send(data);
--- src/pages/expertApplications/index.tsx
+++ src/pages/expertApplications/index.tsx
@@ -1,69 +1,25 @@
 import React, { useEffect, useState } from "react";
-import Image from "next/image";
-import EditProfileModal from "@/components/SharedComponents/EditProfileModal";
-import DeleteModal from "@/components/SharedComponents/DeleteModal";
+import { useSession } from "next-auth/react";
+import ApproveExpertButton from "@/components/TopicComponents/ApproveExpertButton";
+import DenyExpertButton from "@/components/TopicComponents/DenyExpertButton";
 
 export default function ExpertApplications() {
-  const [page, setPage] = useState(1);
-  const [filter, setFilter] = useState("");
-  const [users, setUsers] = useState([]);
+  const { data: session } = useSession();
+  const [expertApplications, setExpertApplications] = useState([]);
 
-  const getUsers = async (page: number, filter: string) => {
-    const data = await fetch(`/api/users/${page}/${filter}`);
-    const userData = await data.json();
-    setUsers(userData);
+  const getExpertApplications = async () => {
+    const data = await fetch(`/api/expertApplications/0`);
+    const expertData = await data.json();
+    setExpertApplications(expertData);
   };
 
   useEffect(() => {
-    getUsers(page, filter);
-  }, [page, filter, users]);
-
-  // query functions
-  const nextPage = () => {
-    if (users.length === 20) setPage(page + 1);
-  };
-
-  const prevPage = () => {
-    if (page > 1) setPage(page - 1);
-  };
-
-  const search = (event: any) => {
-    event.preventDefault();
-    if (event.keyCode == 13) {
-      setFilter(event.target.value);
-      event.target.value = "";
-    }
-  };
+    getExpertApplications();
+  }, [expertApplications]);
 
   return (
     <>
-      <div className="p-4 bg-white block sm:flex items-center justify-between border-b border-gray-200 lg:mt-1.5 dark:bg-gray-800 dark:border-gray-700">
-        <div className="w-full mb-1">
-          <div className="mb-4">
-            <h1 className="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">
-              {filter.length
-                ? "Your search: " + filter
-                : "Expertise Applications"}
-            </h1>
-          </div>
-          <div className="sm:flex">
-            <div className="items-center hidden mb-3 sm:flex sm:divide-x sm:divide-gray-100 sm:mb-0 dark:divide-gray-700">
-              <div className="relative mt-1 lg:w-64 xl:w-96">
-                <input
-                  type="text"
-                  name="email"
-                  id="users-search"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                  placeholder="Search for users"
-                  onKeyUp={search}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* BAR ON TOP OF ALL USERS ^^ ALL USERS BELOW vvvv */}
-      {users.length ? (
+      {expertApplications.length ? (
         <>
           <div className="flex flex-col">
             <div className="overflow-x-auto">
@@ -72,16 +28,11 @@ export default function ExpertApplications() {
                   <table className="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-600">
                     <thead className="bg-gray-100 dark:bg-gray-700">
                       <tr>
-                        <th scope="col" className="p-4">
-                          <div className="flex items-center">
-                            <input
-                              id="checkbox-all"
-                              aria-describedby="checkbox-1"
-                              type="checkbox"
-                              className="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600"
-                            />
-                            <label className="sr-only">checkbox</label>
-                          </div>
+                        <th
+                          scope="col"
+                          className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400"
+                        >
+                          Id
                         </th>
                         <th
                           scope="col"
@@ -93,19 +44,19 @@ export default function ExpertApplications() {
                           scope="col"
                           className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400"
                         >
-                          Email
+                          Topic
                         </th>
                         <th
                           scope="col"
                           className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400"
                         >
-                          Expertise
+                          Topic Id
                         </th>
                         <th
                           scope="col"
                           className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400"
                         >
-                          Authorization
+                          Applied At
                         </th>
                         <th
                           scope="col"
@@ -117,52 +68,42 @@ export default function ExpertApplications() {
                     </thead>
                     <tbody className="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
                       {/* ########## USER DATA BEING MAPPED ########### */}
-                      {users.map((user: User) => {
+                      {expertApplications.map((expert: any) => {
                         return (
                           <tr
                             className="hover:bg-gray-100 dark:hover:bg-gray-700"
-                            key={user.fischerId}
+                            key={expert.id}
                           >
                             <td className="w-4 p-4">
-                              <div className="flex items-center">
-                                <input
-                                  id="checkbox-{{ .id }}"
-                                  aria-describedby="checkbox-1"
-                                  type="checkbox"
-                                  className="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600"
-                                />
-                                <label className="sr-only">checkbox</label>
+                              <div className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                                {expert.user.fischerId}
                               </div>
                             </td>
                             <td className="flex items-center p-4 mr-12 space-x-6 whitespace-nowrap">
-                              <Image
-                                className="w-10 h-10 rounded-full"
-                                src={user.image}
-                                alt="avatar"
-                                width={100}
-                                height={100}
-                              />
                               <div className="text-sm font-normal text-gray-500 dark:text-gray-400">
                                 <div className="text-base font-semibold text-gray-900 dark:text-white">
-                                  {user.name}
-                                </div>
-                                <div className="text-sm font-normal text-gray-500 dark:text-gray-400">
-                                  Fischer ID: {user.fischerId}{" "}
+                                  {expert.user.name}
                                 </div>
                               </div>
                             </td>
                             <td className="max-w-sm p-4 overflow-hidden text-base font-normal text-gray-500 truncate xl:max-w-xs dark:text-gray-400">
-                              {user.email}
+                              {expert.topic.name}
                             </td>
                             <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                              {user.expertise.length}
+                              {expert.topic.id}
                             </td>
                             <td className="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                              {user.isAdmin ? "Admin" : "User"}
+                              {expert.createdAt}
                             </td>
                             <td className="p-4 space-x-2 whitespace-nowrap">
-                              <EditProfileModal {...user} />
-                              <DeleteModal {...user} />
+                              <ApproveExpertButton
+                                fischerId={expert.user.fischerId}
+                                topicId={expert.topic.id}
+                              />
+                              <DenyExpertButton
+                                fischerId={expert.user.fischerId}
+                                topicId={expert.topic.id}
+                              />
                             </td>
                           </tr>
                         );
@@ -174,107 +115,11 @@ export default function ExpertApplications() {
               </div>
             </div>
           </div>
-          <div className="right-0 items-center w-full p-4 bg-white border-t border-gray-200 sm:flex sm:justify-between dark:bg-gray-800 dark:border-gray-700">
-            <div className="flex items-center mb-4 sm:mb-0">
-              <a
-                onClick={prevPage}
-                className="inline-flex justify-center p-1 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              >
-                <svg
-                  className="w-7 h-7"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </a>
-              <a
-                onClick={nextPage}
-                className="inline-flex justify-center p-1 mr-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              >
-                <svg
-                  className="w-7 h-7"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </a>
-            </div>
-            <div className="flex items-center space-x-3">
-              <button
-                onClick={prevPage}
-                className="inline-flex items-center justify-center flex-1 px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
-                <svg
-                  className="w-5 h-5 mr-1 -ml-1"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                Previous
-              </button>
-              <button
-                onClick={nextPage}
-                className="inline-flex items-center justify-center flex-1 px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
-                Next
-                <svg
-                  className="w-5 h-5 ml-1 -mr-1"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </button>
-            </div>
-          </div>
         </>
       ) : (
         <>
           <div className="flex">
-            <h1>No results to be shown</h1>
-          </div>
-          <div className="flex">
-            <button
-              onClick={prevPage}
-              className="inline-flex items-center justify-center flex-1 px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-            >
-              <svg
-                className="w-5 h-5 mr-1 -ml-1"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              Previous
-            </button>
+            <h1>No expert applications to be shown</h1>
           </div>
         </>
       )}
